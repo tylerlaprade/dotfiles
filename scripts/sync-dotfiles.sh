2baea05c732be83d7d4771bdec16d5f48d3b934c
Tyler Laprade, CFA <pinkmonkeyblue@gmail.com>
2026-03-17 12:13:12 -0400

    Include key (un)bindings

--- scripts/sync-dotfiles.sh
+++ scripts/sync-dotfiles.sh
@@ -86,6 +86,9 @@ for item in "$DOTFILES"/.[!.]*; do
   fi
 done
 
+# ~/Library/KeyBindings
+link_tree "$DOTFILES/.config/KeyBindings" "$HOME/Library/KeyBindings"
+
 # scripts -> ~/.local/bin
 mkdir -p ~/.local/bin
 for script in "$DOTFILES"/scripts/*.sh; do
